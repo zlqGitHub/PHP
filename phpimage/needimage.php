27071d5e1e96f24c22cb860da83175fce68cfc86
zlqGitHub <1509874021@qq.com>
2018-10-22 15:12:45 +0800

    链接PHP图像

--- phpimage/needimage.php
+++ phpimage/needimage.php
@@ -5,7 +5,10 @@
 	<title>获取PHP文件中生成的图片</title>
 </head>
 <body>
-	<a href="phpimagecreate.php">获取生成的图片</a>
+	<a href="phpimagecreate.php">获取生成的简单图片</a><br>
+	<a href="phpimagecreatefiel.php">获取生成的图像文件</a><br>
+	<a href="readimagefieltext.php">图片水印</a><br>
+	<a href="font.php">TrueType字体</a>
 	
 </body>
 </html>
