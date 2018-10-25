f15e82c2b6437d8693dc17914c208f41712b020f
zlqGitHub <1509874021@qq.com>
2018-10-25 21:06:44 +0800

    PHP与web页面交互

---
+++ phpweb/formdemohandler.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+		if($_SERVER['REQUEST_METHOD'] == "POST"){
+			echo "POST方式提交！<br>";
+		}else{
+			echo "GET方式提交！<br>";
+		}
+
+		$name = $_POST['name'];
+		echo $name."<br>";
+
+		//复选框处理
+		/*  先去判断是否选中，否则未选中时报错
+		$achecked = $_POST['achecked'];
+		$bchecked = $_POST['bchecked'];
+		$cchecked = $_POST['cchecked'];
+		*/
+	
+		if(isset($_POST['achecked'])){
+			$achecked = $_POST['achecked'];
+		}
+		if(isset($_POST['bchecked'])){
+			$achecked = $_POST['bchecked'];
+		}
+		if(isset($_POST['cchecked'])){
+			$achecked = $_POST['cchecked'];
+		}
+	
+		if(isset($achecked) && $achecked == 'A'){
+			echo "A选项被选中<br>";
+		}else{
+			echo "A选项未被选中<br>";
+		}
+		if(isset($bchecked) && $bchecked == 'A'){
+			echo "B选项被选中<br>";
+		}else{
+			echo "B选项未被选中<br>";
+		}
+		if(isset($cchecked) && $cchecked == 'A'){
+			echo "C选项被选中<br>";
+		}else{
+			echo "C选项未被选中<br>";
+		}
+
+		//单选框处理
+		$radio = $_POST['radio'];
+		if($radio  == "蓝天"){
+			echo "选中了蓝天<br>";
+		}
+		if($radio  == "白云"){
+			echo "选中了白云<br>";
+		}
+		if($radio  == "大海"){
+			echo "选中了大海<br>";
+		}
+
+
+	?>
+	
+</body>
+</html>
