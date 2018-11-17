4122215da61cc2cdcbd36313ea95367bda97c1d7
zlqGitHub <1509874021@qq.com>
2018-11-17 14:08:16 +0800

    论坛版权文件

---
+++ BBS/inc/foot.php
@@ -0,0 +1,16 @@
+<table width="98%" border="0" align="center">
+<!--论坛版权文件-->
+    <tr>
+        <td height="10"></td>
+    </tr>
+    <tr>
+        <td height="10" bgcolor="#5F8AC5"></td>
+    </tr>
+    <tr>
+        <td height="40" align="center" valign="middle" bgcolor="#f0b604">
+            建议使用浏览器 IE 8.0 以上分辨率 1024*768 以上<br>
+            版权所有：<a href="http://www.zlqstudy.com" target="_parent">迅捷 BBS</a>
+        </td>
+    </tr>
+
+</table>
