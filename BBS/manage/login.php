d0f78dc1ee238f8ce02ee5f9445136c03c70bc17
zlqGitHub <1509874021@qq.com>
2018-11-19 22:30:10 +0800

    管理用户的登录页面

---
+++ BBS/manage/login.php
@@ -0,0 +1,10 @@
+<?php
+    include "../inc/mysql.inc";
+    include "../inc/myfunction.inc";
+    $aa = new mysql();
+    $bb = new myfunction();
+    $aa->link("");
+    $_SESSION['manage_name'] = "";
+    $_SESSION['manage_tag'] = "";
+
+?>
