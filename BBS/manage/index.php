4536219192eec43ad35a48eb700db5f142c6fdd3
zlqGitHub <1509874021@qq.com>
2018-11-20 21:28:33 +0800

    后台管理主页面(修改)

--- BBS/manage/index.php
+++ BBS/manage/index.php
@@ -1,22 +1,16 @@
 <?php
-    include "session.inc";
+include "session.inc";
 ?>
-<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv=Content-Type content="text/html" charset="gb2312">
     <title>迅捷 BBS 系统-后台管理</title>
 </head>
-<frameset id="frame" border="false" frameSpacing="0" rows="*" cols="200" scrolling="yes">
-    <frame name="left" marginwidth="0" marginheight="0" src="index_left.php" scrolling="yes">
-    <frameset border="false" frameSpacing="0" rows="53" frameBorder="0" cols="*" scrolling="yes" >
-        <frame name="top" src="index_left.php" scrolling="no">
-        <frame name="main" src="index_right.php">
-    </frameset>
 
-</frameset>
+<FRAMESET id=frame border- false frameSpacing- -0 rows=* frameBorder 0 cols= 200,* scrolling- "yes">
 
+<FRAME name =left marginWidth=0 marginHeight0 src- "index_left.php" scrolling-yes>
 
-</html>
+<FRAMESET border- false frameSpacing-0 rows=53,* frameBorder-0 cols=* scrolling-"yes">
+
+<FRAME name . top sre "index_ top.php" scrolling -no><FRAME name=main src="index_right.php"></FRAMESET></FRAMESET>
