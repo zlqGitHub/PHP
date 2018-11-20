1721f67527b7f514f7ae46b7f2aa46a89095f16a
zlqGitHub <1509874021@qq.com>
2018-11-20 17:40:28 +0800

    管理员用户的登录页面

--- BBS/manage/login.php
+++ BBS/manage/login.php
@@ -6,7 +6,6 @@
     $aa->link("");
     $_SESSION['manage_name'] = "";
     $_SESSION['manage_tag'] = "";
-
 ?>
 
 <head>
@@ -17,43 +16,50 @@
             }
         -->
     </style>
-    <title>:::管理员登录==迅捷 BBS 系统:::</title>
+    <title>管理员登录==迅捷 BBS 系统</title>
 </head>
 
 <body>
     <br>
-    <div align="center">
-        <center>
-            <form action="auth.php">
-                <table border="1" bordercolor="#111111" width="240">
-                    <tr>
-                        <td width="238" colspan="2" height="25" bgcolor="#A8A3AD">
-                            <p align="center"><b><font color="#FFFFFF">迅捷 BBS 后台管理系统</font></b></p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="64" height="26" bgcolor="#E3E1E6">
-                            <p align="center">账&nbsp;号：</p>
-                        </td>
-                        <td height="26" width="173">
-                            <input type="text" name="username" size="20" style="color: #A8A3AD;border-style: solid;border-width: 1;padding-left: 4;padding-top: 1;padding-bottom: 1;">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="64" height="26" bgcolor="#E3E1E6">
-                            <p align="center">密&nbsp;码：</p>
-                        </td>
-                        <td height="26" width="173">
-                            <input type="password" name="password" size="20" style="color: #A8A3AD;border-style: solid;border-width: 1;padding-left: 4;padding-top: 1;padding-bottom: 1;">
-                        </td>
-                    </tr>
+    <center>
+        <form action="auth.php" method="post" name="tijiao">
+            <table border="1" bordercolor="#111111" width="240">
+                <tr>
+                    <td width="238" colspan="2" height="25" bgcolor="#A8A3AD">
+                        <p align="center"><b><font color="#FFFFFF">迅捷 BBS 后台管理系统</font></b></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="64" height="26" bgcolor="#E3E1E6">
+                        <p align="center">账&nbsp;号：</p>
+                    </td>
+                    <td height="26" width="173">
+                        <input type="text" name="username" size="20" style="color: #A8A3AD;border-style: solid;border-width: 1;padding-left: 4;padding-top: 1;padding-bottom: 1;">
+                    </td>
+                </tr>
+                <tr>
+                    <td width="64" height="26" bgcolor="#E3E1E6">
+                        <p align="center">密&nbsp;码：</p>
+                    </td>
+                    <td height="26" width="173">
+                        <input type="password" name="password" size="20" style="color: #A8A3AD;border-style: solid;border-width: 1;padding-left: 4;padding-top: 1;padding-bottom: 1;">
+                    </td>
+                </tr>
+                <tr>
+                    <td width="238" colspan="2" bgcolor="#E3E1E6" height="29">
+                        <p align="center">
+                            <input type="submit" name="login" value="登录">&nbsp;&nbsp;&nbsp;&nbsp;
+                            <input type="reset" name="cancel" value="取消">
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="238" height="26" colspan="2" bgcolor="#A8A3AD"></td>
+                </tr>
+            </table>
 
-
-                </table>
-
-            </form>
-        </center>
-    </div>
+        </form>
+    </center>
 
 </body>
 
