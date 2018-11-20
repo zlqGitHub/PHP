06a43986ffe09c44f7eab5efed890b8fb7c22cd2
zlqGitHub <1509874021@qq.com>
2018-11-20 09:55:13 +0800

    管理员用户的登录页面

--- BBS/manage/login.php
+++ BBS/manage/login.php
@@ -8,3 +8,54 @@
     $_SESSION['manage_tag'] = "";
 
 ?>
+
+<head>
+    <style>
+        <!--
+            td{
+                font-size: 10pt;
+            }
+        -->
+    </style>
+    <title>:::管理员登录==迅捷 BBS 系统:::</title>
+</head>
+
+<body>
+    <br>
+    <div align="center">
+        <center>
+            <form action="auth.php">
+                <table border="1" bordercolor="#111111" width="240">
+                    <tr>
+                        <td width="238" colspan="2" height="25" bgcolor="#A8A3AD">
+                            <p align="center"><b><font color="#FFFFFF">迅捷 BBS 后台管理系统</font></b></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="64" height="26" bgcolor="#E3E1E6">
+                            <p align="center">账&nbsp;号：</p>
+                        </td>
+                        <td height="26" width="173">
+                            <input type="text" name="username" size="20" style="color: #A8A3AD;border-style: solid;border-width: 1;padding-left: 4;padding-top: 1;padding-bottom: 1;">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="64" height="26" bgcolor="#E3E1E6">
+                            <p align="center">密&nbsp;码：</p>
+                        </td>
+                        <td height="26" width="173">
+                            <input type="password" name="password" size="20" style="color: #A8A3AD;border-style: solid;border-width: 1;padding-left: 4;padding-top: 1;padding-bottom: 1;">
+                        </td>
+                    </tr>
+
+
+                </table>
+
+            </form>
+        </center>
+    </div>
+
+</body>
+
+
+
