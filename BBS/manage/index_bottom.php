f693143c75bc0b93f9f6f611daa12220dfcf4215
zlqGitHub <1509874021@qq.com>
2018-11-20 20:56:40 +0800

    后台管理主页面的底部页面

---
+++ BBS/manage/index_bottom.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!--后台管理页面的底部页面-->
+<table class="border" cellspacing="1" cellpadding="2" width="100%" align="center" border="0">
+    <tbody>
+        <tr align="middle">
+            <td class="topbg" height="25">
+                <span class="Glow">
+                    Copyright 2018 &copy;
+                    <a href="http://www.baidu.com" target="_blank"><font color="#FFF000"> 迅捷 BBS </font></a>
+                    All Rights Reserved.
+                </span>
+            </td>
+        </tr>
+
+    </tbody>
+</table>
+
+</body>
+</html>
