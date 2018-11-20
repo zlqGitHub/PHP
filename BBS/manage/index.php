d67eb0e43a60c9d1648f906c1fdba191710845f6
zlqGitHub <1509874021@qq.com>
2018-11-20 21:13:35 +0800

    后台管理主页面

---
+++ BBS/manage/index.php
@@ -0,0 +1,22 @@
+<?php
+    include "session.inc";
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>迅捷 BBS 系统-后台管理</title>
+</head>
+<frameset id="frame" border="false" frameSpacing="0" rows="*" cols="200" scrolling="yes">
+    <frame name="left" marginwidth="0" marginheight="0" src="index_left.php" scrolling="yes">
+    <frameset border="false" frameSpacing="0" rows="53" frameBorder="0" cols="*" scrolling="yes" >
+        <frame name="top" src="index_left.php" scrolling="no">
+        <frame name="main" src="index_right.php">
+    </frameset>
+
+</frameset>
+
+
+</html>
