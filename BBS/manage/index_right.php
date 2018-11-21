9495b35a3e1a478271850c6dc060b1102bf6f954
zlqGitHub <1509874021@qq.com>
2018-11-21 11:25:58 +0800

    后台管理主页面的右侧页面

---
+++ BBS/manage/index_right.php
@@ -0,0 +1,86 @@
+<?php
+
+?>
+
+<head>
+    <link rel="stylesheet" href="left.css" type="text/css">
+</head>
+<table width="100%">
+    <tr>
+        <td width="20" rowspan="2">&nbsp;</td>
+        <td class="topbg" align="middle" width="100">论坛版块管理</td>
+        <td width="300">&nbsp;</td>
+        <td width="40" rowspan="2">&nbsp;</td>
+        <td class="topbg" align="middle" width="100">论坛用户管理</td>
+        <td width="300">&nbsp;</td>
+        <td width="21" rowspan="2">&nbsp;</td>
+    </tr>
+    <tr class="topbg2">
+        <td colspan="2" height="1"></td>
+        <td colspan="2" height="1"></td>
+    </tr>
+</table>
+
+<table width="100%">
+    <tr>
+        <td width="20">&nbsp;</td>
+        <td width="400"><br>
+            &nbsp;&nbsp;本管理模块共分为四个子模块：父版块添加、父版块管理、子版块添加、子版块管理。
+            其中父版块添加和子版块天机可以实现本论坛的父版块和子版块的添加；
+            父版块的管理和子版块的管理可以实现本论坛父版块和子版块的删除和编辑等功能。<br>
+        </td>
+
+        <td width="40">&nbsp;</td>
+        <td width="400"><br>
+            本管理模块共分为四个子模块：父版块添加、父版块管理、子版块添加、子版块管理。
+            其中父版块添加和子版块天机可以实现本论坛的父版块和子版块的添加；
+            父版块的管理和子版块的管理可以实现本论坛父版块和子版块的删除和编辑等功能。<br>
+        </td>
+
+        <td width="21">&nbsp;</td>
+    </tr>
+
+</table>
+
+<table width="100%">
+    <tr>
+        <td width="20" rowspan="2">&nbsp;</td>
+        <td class="topbg" align="middle" width="100">安全管理</td>
+        <td width="300">&nbsp;</td>
+        <td width="40" rowspan="2">&nbsp;</td>
+        <td class="topbg" align="middle" width="100">系统信息</td>
+        <td width="300">&nbsp;</td>
+        <td width="21" rowspan="2">&nbsp;</td>
+    </tr>
+    <tr class="topbg2" >
+        <td colspan="2" height="1"></td>
+        <td colspan="2"></td>
+    </tr>
+</table>
+
+<table width="100%">
+    <tr>
+        <td width="20">&nbsp;</td>
+        <td width="400"><br>
+            本管理模块共分为四个子模块：父版块添加、父版块管理、子版块添加、子版块管理。
+            其中父版块添加和子版块天机可以实现本论坛的父版块和子版块的添加；
+            父版块的管理和子版块的管理可以实现本论坛父版块和子版块的删除和编辑等功能。<br>
+        </td>
+
+        <td width="40">&nbsp;</td>
+        <td width="400"><br>
+            本管理模块共分为四个子模块：父版块添加、父版块管理、子版块添加、子版块管理。
+            其中父版块添加和子版块天机可以实现本论坛的父版块和子版块的添加；
+            父版块的管理和子版块的管理可以实现本论坛父版块和子版块的删除和编辑等功能。<br>
+        </td>
+
+        <td width="21">&nbsp;</td>
+    </tr>
+
+</table>
+
+
+
+<?php
+    include "index_bottom.php";
+?>
