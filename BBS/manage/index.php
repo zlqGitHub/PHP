f70b037d1c416574b4be390ed98fc0a7a9c0515e
zlqGitHub <1509874021@qq.com>
2018-11-21 10:28:16 +0800

    后台管理的主页面

--- BBS/manage/index.php
+++ BBS/manage/index.php
@@ -1,16 +1,20 @@
 <?php
-include "session.inc";
+    include "session.inc";
 ?>
 
+<html>
 <head>
-    <meta http-equiv=Content-Type content="text/html" charset="gb2312">
-    <title>迅捷 BBS 系统-后台管理</title>
+    <meta charset="UTF-8">
+
+    <title>Document</title>
 </head>
+    <frameset id="frame" cols="200,*" scrolling="yes">
+        <frame name="left" src="index_left.php" scrolling="yes">
+        <frameset rows="53,*,50" cols="*" scrolling="yes">
+            <frame name="top" src="index_top.php" scrolling="no">
+            <frame name="main" src="index_right.php">
+            <frame name="bottom" src="index_bottom.php">
+        </frameset>
 
-<FRAMESET id=frame border- false frameSpacing- -0 rows=* frameBorder 0 cols= 200,* scrolling- "yes">
-
-<FRAME name =left marginWidth=0 marginHeight0 src- "index_left.php" scrolling-yes>
-
-<FRAMESET border- false frameSpacing-0 rows=53,* frameBorder-0 cols=* scrolling-"yes">
-
-<FRAME name . top sre "index_ top.php" scrolling -no><FRAME name=main src="index_right.php"></FRAMESET></FRAMESET>
+    </frameset>
+</html>
