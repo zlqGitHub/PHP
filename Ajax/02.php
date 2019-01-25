68870e59770254bacaf157189fdaaed0a3d9d4c7
zlqGitHub <1509874021@qq.com>
2019-01-25 13:04:33 +0800

    post请求处理

--- Ajax/02.php
+++ Ajax/02.php
@@ -1,7 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lenovo
- * Date: 2019/1/25
- * Time: 10:41
- */
+header('content-type:text/html,charset=utf-8');
+//超全局变量：https://www.runoob.com/php/php-superglobals.html
+print_r($_POST);
+echo $_POST['userName'];
+echo $_POST['userPass'];
+?>
