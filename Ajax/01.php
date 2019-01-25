8c30c65e9519e96bced8dea2d345d8bdcde89667
zlqGitHub <1509874021@qq.com>
2019-01-25 13:00:54 +0800

    get请求处理

--- Ajax/01.php
+++ Ajax/01.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lenovo
- * Date: 2019/1/25
- * Time: 10:22
- */
+header('content-type:text/html,charset=utf-8');
+//超全局变量：https://www.runoob.com/php/php-superglobals.html
+//  $_GET 接收到的值是个数组
+print_r($_GET);
+echo $_GET['userName'];
+echo $_GET['userPass'];
+?>
